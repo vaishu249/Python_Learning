0c16725a6c1949a489ad971894ca565304e37ab3
vaishu1109 <vaishu.sg1109@gmail.com>
2025-03-31 22:00:16 +0530

    String slicing

--- String/P1.py
+++ String/P1.py
@@ -1,2 +1,6 @@
 message="hello World"
 print(message)
+my_message='barbie\'s world'
+print(my_message)
+print(len(my_message))
+print(my_message[6])
---
+++ String/P2StringSlicing.py
@@ -0,0 +1,9 @@
+message="A general statement, in the context of writing or communication"
+print(message[0:5])
+print(message[:5])
+print(message[0:])
+print(message[:])
+print(message[::-1])
+
+new_message="noitacinummoc ro gnitirw fo txetnoc eht ni ,tnemetats lareneg A"
+print(message[::-1]==new_message)
